dcfd153fe370b59dffc2a94d1724a73f7a262745
Tushar Tomar <tomartushar5@gmail.com>
2022-05-13 14:24:08 +0530

    Added Main Display Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+import MainDisplay from "./components/MainDisplay";
 
 function App() {
   return (
-    <div> Courses </div>
+    <div className="flex flex-col">
+      <Header />
+      <MainDisplay />
+    </div>
   );
 }
 
---
+++ src/data/mainDisplayData.js
@@ -0,0 +1,15 @@
+export const data = {
+  courseImage:
+    "https://d1vwxdpzbgdqj.cloudfront.net/assets/dsba-domestic/dsba-pp-bg-159231d47e01da6d29eff4a3a16b9bf7966be3f0648118171f7346d83c0b00d6.jpg",
+  courseLogo:
+    "https://d1vwxdpzbgdqj.cloudfront.net/assets/learning-partners/new-logo/utaustin-1-d74dd3fcbc8f8d1a2d9b77e04a20801f1ab1fdbc225a6b198f752d905d6a2bc8.png",
+  mainHeading:
+    "Build a successful career in Data Science and Business Analytics",
+  courseName: "PGP - Data Science and Business Analytics",
+  extraImage:
+    "https://d1vwxdpzbgdqj.cloudfront.net/assets/version-feature/new-ui-vbanner-117f19c1edbb9a9649471797c35c8b1ca4caf277331bc691026513765af6c349.png",
+  rating: "580",
+  months: "11",
+  detail: "Online Mentorship",
+  closeData: "15th May 2022",
+};
